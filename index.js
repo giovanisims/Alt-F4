f1e3146683b5e04d1d25358d7664a244c6dd91d9
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2024-10-28 17:43:04 -0300

    Connection with the database

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 let express = require('express');
 let app = express();
 
+app.use(express.static('./pages'))
+
 const port = 3000;
 
-app.use(express.static('./pages'))
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "puc@123",
+    database: "altf4",
+    port: 3305
+})
+
+con.connect(function(err){
+    if(err) throw err;
+    console.log("Connected");
+})
+
+app.get('/products', (req, res) => {
+    const query = "SELECT * FROM product";
+
+    con.query(query, (err, results) => {
+        if(err){
+            console.error('Erro ao buscar produtos', err);
+            res.status(500).send("Erro no servidor");
+        }else{
+            res.json(results);
+        }
+    })
+})
 
 app.get ('', (req, res) => {
     res.send('index');
