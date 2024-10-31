b4b7709c191b506dcabacf173c21f3a9f4ef59b7
Livia Rosembach Oliveira <rosembach.livia@gmail.com>
2024-10-30 21:45:51 -0300

    Added specific fetch

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "puc@123",
+    password: "Lili2209*",
     database: "altf4",
     port: 3305
 })
 
-con.connect(function(err){
-    if(err) throw err;
+con.connect(function (err) {
+    if (err) throw err;
     console.log("Connected");
 })
 
@@ -23,16 +23,42 @@ app.get('/products', (req, res) => {
     const query = "SELECT * FROM product";
 
     con.query(query, (err, results) => {
-        if(err){
+        if (err) {
             console.error('Erro ao buscar produtos', err);
             res.status(500).send("Erro no servidor");
-        }else{
+        } else {
             res.json(results);
         }
     })
 })
 
-app.get ('', (req, res) => {
+app.get('/productsPC', (req, res) => {
+    const query = "SELECT p.Name, p.Rating, p.Price, p.Stock FROM product as p JOIN device as d ON p.ProductID = d.ProductID WHERE d.Type = 'PC'; ";
+
+    con.query(query, (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar produtos', err);
+            res.status(500).send("Erro no servidor");
+        } else {
+            res.json(results);
+        }
+    })
+})
+
+app.get('/productsMobile', (req, res) => {
+    const query = "SELECT p.Name, p.Rating, p.Price, p.Stock FROM product as p JOIN device as d ON p.ProductID = d.ProductID WHERE d.Type = 'Mobile'; ";
+
+    con.query(query, (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar produtos', err);
+            res.status(500).send("Erro no servidor");
+        } else {
+            res.json(results);
+        }
+    })
+})
+
+app.get('', (req, res) => {
     res.send('index');
 })
 
